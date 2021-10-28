30cfef8205e6483d1afb8c09be6b9de48615c31b
lampham <phamlamuet@gmail.com>
2021-10-28 14:20:02 +0700

    add Trending section

--- medius-frontend/src/App.js
+++ medius-frontend/src/App.js
@@ -1,6 +1,7 @@
 import { Button, Container, Grid } from '@material-ui/core';
 import './App.scss';
 import NavBar from './components/NavBar';
+import TrendingUpIcon from '@mui/icons-material/TrendingUp';
 
 function App() {
   return (
@@ -8,19 +9,24 @@ function App() {
       <NavBar />
       <div className="App_MainSection">
         <Container>
-        <Grid container justify="center" alignItems="center">
-          <Grid item lg={6}>
-            <h1>Where good idea find you</h1>
-            <p>
-              Read and share new perspective on just about any topic.
-              Everyone's welcome.
-            </p>
-            <Button>Get started</Button>
+          <Grid container justify="center" alignItems="center">
+            <Grid item lg={6}>
+              <h1>Where good idea find you</h1>
+              <p>
+                Read and share new perspective on just about any topic.
+                Everyone's welcome.
+              </p>
+              <Button>Get started</Button>
+            </Grid>
+            <Grid item lg={6}>
+              <img src="https://cdn-static-1.medium.com/sites/medium.com/creators/images/Illustrations-Dimensional-1.png" alt="" />
+            </Grid>
           </Grid>
-          <Grid item lg={6}>
-            <img src="https://cdn-static-1.medium.com/sites/medium.com/creators/images/Illustrations-Dimensional-1.png" alt=""/>
-          </Grid>
-        </Grid>
+        </Container>
+      </div>
+      <div className="Trending">
+        <Container>
+          <h2><span><TrendingUpIcon/></span>Trending on Medius</h2>
         </Container>
       </div>
     </div>
