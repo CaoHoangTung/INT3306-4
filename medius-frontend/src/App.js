5ad7bbd208dbd0a4ab71501c4dcb3fa45aa0f43e
lampham <phamlamuet@gmail.com>
2021-10-30 19:24:12 +0700

    fix some bug

--- medius-frontend/src/App.js
+++ medius-frontend/src/App.js
@@ -5,8 +5,10 @@ import Footer from './components/Footer';
 import MediumPosts from './components/PostPreview'
 import Post from './components/Post'
 import TrendingUpIcon from '@mui/icons-material/TrendingUp';
-
+import React, { useState } from 'react';
+import LoginModal from './components/LoginModal'
 function App() {
+  const [show, setShow] = useState(false);
   return (
     <div className="App">
       <NavBar />
@@ -19,7 +21,7 @@ function App() {
                 Read and share new perspective on just about any topic.
                 Everyone's welcome.
               </p>
-              <Button>Get started</Button>
+              <Button onClick={() => setShow(true)}>Get started</Button>
             </Grid>
             <Grid item lg={6}>
               <img src="https://cdn-static-1.medium.com/sites/medium.com/creators/images/Illustrations-Dimensional-1.png" alt="" />
@@ -27,45 +29,48 @@ function App() {
           </Grid>
         </Container>
       </div>
+      <div>
+        <LoginModal onClose={()=>setShow(false)} show={show}/>
+      </div>
       <div className="Trending">
         <Container>
-          <h2><span><TrendingUpIcon/></span>Trending on Medius</h2>
+          <h2><span><TrendingUpIcon /></span>Trending on Medius</h2>
           <MediumPosts
-              author = "author"
-              topic = "topic"
-              title = "title"
-              contentPreview = "contentPreview"
-              postTime = "2:00:00 PM 10/29/2021"
+            author="author"
+            topic="topic"
+            title="title"
+            contentPreview="contentPreview"
+            postTime="2:00:00 PM 10/29/2021"
           />
-            <MediumPosts
-                author = "author"
-                topic = "topic"
-                title = "title"
-                contentPreview = "contentPreview"
-                postTime = "2:00:00 PM 10/29/2021"
-            />
-            <MediumPosts
-                author = "author"
-                topic = "topic"
-                title = "title"
-                contentPreview = "contentPreview"
-                postTime = "2:00:00 PM 10/29/2021"
-            />
-            <MediumPosts
-                author = "author"
-                topic = "topic"
-                title = "title"
-                contentPreview = "contentPreview"
-                postTime = "2:00:00 PM 10/29/2021"
-            />
-            <Post
-                date = "2:00:00 PM 10/29/2021"
-                author = "author"
-                description = "12345678906542321937581793479217380472314712730497398274891072347213749712930470231947730247239482713094"
-                image = "logo512.png"
-                imageLabel = "imageLabel"
-                title = "title"
-            />
+          <MediumPosts
+            author="author"
+            topic="topic"
+            title="title"
+            contentPreview="contentPreview"
+            postTime="2:00:00 PM 10/29/2021"
+          />
+          <MediumPosts
+            author="author"
+            topic="topic"
+            title="title"
+            contentPreview="contentPreview"
+            postTime="2:00:00 PM 10/29/2021"
+          />
+          <MediumPosts
+            author="author"
+            topic="topic"
+            title="title"
+            contentPreview="contentPreview"
+            postTime="2:00:00 PM 10/29/2021"
+          />
+          <Post
+            date="2:00:00 PM 10/29/2021"
+            author="author"
+            description="12345678906542321937581793479217380472314712730497398274891072347213749712930470231947730247239482713094"
+            image="logo512.png"
+            imageLabel="imageLabel"
+            title="title"
+          />
         </Container>
       </div>
       <Footer />
--- medius-frontend/src/components/Footer.js
+++ medius-frontend/src/components/Footer.js
@@ -4,12 +4,12 @@ const Footer = () => {
     return(
         <div className="Footer">
             <Container>
-                <div className="NavBar_Container">
+                <div className="Footer_Container">
                     <img
                         src="https://miro.medium.com/max/8978/1*s986xIGqhfsN8U--09_AdA.png"
                         alt="logo"
                     />
-                    <div className="NavBar_Lists">
+                    <div className="Footer_Lists">
                         <ul>
                             <li>About us</li>
                             <li>Help</li>
