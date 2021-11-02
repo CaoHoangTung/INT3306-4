5a4a6f7c59dd6a7aa69c4c2894d2d07d7a5488c0
minhquanym@gmail.com <minhquanym@gmail.com>
2021-11-02 21:44:40 +0700

    Add topic, postsTopic, usersPosts frontend api

---
+++ medius-frontend/src/api/users_posts.js
@@ -0,0 +1,30 @@
+import API from "./api";
+
+export async function getUserPostAll() {
+    const response = await API.get("/userpost/all");
+    return response?.data;
+}
+
+export async function getUserPost(userId, postId) {
+    const response = await API.get(`/userpost/view?user_id=${userId}&post_id=${postId}`);
+    return response?.data;
+}
+
+export async function createUserPost(userpost) {
+    const response = await API.post("/userpost/create", userpost);
+    return response?.data;
+}
+
+export async function updateUserPost(userpost) {
+    const response = await API.post("/userpost/update", userpost);
+    return response?.data;
+}
+
+export async function deleteUserPost(userId, postId) {
+    var body = {
+        user_id: userId,
+        post_id: postId
+    };
+    const response = await API.post("/userpost/delete", body);
+    return response?.data;
+}
