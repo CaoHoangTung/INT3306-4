d024ee6333af60cba34288c6143f9828f7652827
LuckyBoy <bachb0952001@gmail.com>
2021-11-19 14:05:20 +0700

    create viewPost folder

---
+++ medius-frontend/src/pages/viewPost/index.js
@@ -0,0 +1,18 @@
+import './style.scss'
+
+function viewPost(props) {
+    return (
+        <div className="viewPost">
+            <div  className="header">
+
+            </div>
+            <div className="content">
+
+            </div>
+            <div className="footer">
+
+            </div>
+        </div>
+    );
+}
+export default viewPost;
