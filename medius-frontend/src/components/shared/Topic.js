c4ff31f70c3c46709be2cebb262cbc6a5e794d95
lampham <phamlamuet@gmail.com>
2021-11-28 00:32:48 +0700

    change css whotofollow button

--- medius-frontend/src/components/shared/Topic.js
+++ medius-frontend/src/components/shared/Topic.js
@@ -1,21 +1,23 @@
 import * as React from 'react';
 import Button from '@mui/material/Button';
 
-const Topic = ({topic, link}) => {
+const Topic = ({ topic, link }) => {
     return (
         <Button sx={{
-            ":hover" : {
-                backgroundColor: 'rgba(230,230,230)', 
+            ":hover": {
+                backgroundColor: 'rgba(230,230,230)',
             },
             backgroundColor: 'rgba(242, 242, 242, 1)',
             borderRadius: '100px',
             margin: '5px',
-            padding: '8px 16px',
-            color: 'black'
-        }} variant="contained" href={link}>
+            color: 'black',
+            border: 'none',
+            outline: 'none',
+        }}
+            variant="contained" href={link}>
             {topic}
         </Button>
-            
+
     );
 }
 export default Topic;
