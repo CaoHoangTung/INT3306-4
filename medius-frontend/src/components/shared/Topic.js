052ffb5adfa0c7c0835709161da7b5306fb0a8e5
lampham <phamlamuet@gmail.com>
2021-11-28 00:10:13 +0700

    fix topic ui

--- medius-frontend/src/components/shared/Topic.js
+++ medius-frontend/src/components/shared/Topic.js
@@ -4,14 +4,18 @@ import Button from '@mui/material/Button';
 const Topic = ({topic, link}) => {
     return (
         <Button sx={{
-            color: 'black',
-            backgroundColor: 'rgba(242, 242, 242, 1)',
             ":hover" : {
-                backgroundColor: 'rgba(41, 41, 41, 0.5)'       
-            }
+                backgroundColor: 'rgba(230,230,230)', 
+            },
+            backgroundColor: 'rgba(242, 242, 242, 1)',
+            borderRadius: '100px',
+            margin: '5px',
+            padding: '8px 16px',
+            color: 'black'
         }} variant="contained" href={link}>
             {topic}
         </Button>
+            
     );
 }
 export default Topic;
