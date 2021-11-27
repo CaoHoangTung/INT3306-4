bf75e74b5a430ee74cfeebab428c831835c4d81f
lampham <phamlamuet@gmail.com>
2021-11-28 02:12:19 +0700

    add input to newpost

--- medius-frontend/src/pages/newpost/index.js
+++ medius-frontend/src/pages/newpost/index.js
@@ -4,11 +4,30 @@ import TextEditor from "./TextEditor.js";
 import React from 'react';
 import EditorContainer from "./EditorContainer.js";
 import './newpost.scss';
+import { TextField } from "@mui/material";
 function WritePost() {
     return (
         <div>
             {<NewPostNavBar />}
             <Container>
+                <TextField
+                    label='Title'
+                    placeholder='Enter Tile'
+                    fullWidth
+                    variant="standard"
+                />
+                <TextField 
+                    label='Post preview'
+                    placeholder='Write somethings about your post'
+                    fullWidth
+                    variant="standard"
+                />
+                <TextField
+                    label='Topics'
+                    placeholder='Add some topics to your post'
+                    variant="standard"
+                    fullWidth
+                />
                 <TextEditor />
             </Container>
         </div>
