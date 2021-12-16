3ab75fd176627c39a8c029954042b9959ac5b0bc
WelToHackerLand <krgaim10@gmail.com>
2021-12-16 09:25:07 +0700

    add userdetail for post api

--- medius-server/api/v1/endpoints/posts.py
+++ medius-server/api/v1/endpoints/posts.py
@@ -51,7 +51,13 @@ def view_all_posts(db: Session = Depends(deps.get_db), user_id: str = Query(None
     if not isinstance(posts, List):
         raise HTTPException(status_code=500, detail=msg.DATABASE_ERROR)
 
-    return posts
+    schemas_posts = []
+    for post in posts: 
+        schemas_post = schemas.Post.from_orm(post)
+        schemas_post.get_user_detail(db=db)
+        schemas_posts.append(schemas_post)
+            
+    return schemas_posts    
 
 @router.get("/saved-posts", response_model=List[schemas.Post])
 def view_saved_posts(db: Session = Depends(deps.get_db), current_user: models.User = Depends(deps.get_current_user)) -> Any:
@@ -60,20 +66,15 @@ def view_saved_posts(db: Session = Depends(deps.get_db), current_user: models.Us
     
     if not isinstance(posts, List):
         raise HTTPException(status_code=500, detail=msg.DATABASE_ERROR)
-    
-    return posts
 
-# @router.get("/all/{user_id}", response_model=List[schemas.Post])
-# def view_all_posts(db: Session = Depends(deps.get_db), user_id: str = None, current_user: models.User = Depends(deps.get_current_user)) -> Any:
-#     """
-#     Get all posts with user_id 
-#     """
-#     posts = crud.post.get_by_user_id(db=db, user_id=user_id)
-
-#     if not isinstance(posts, List):
-#         raise HTTPException(status_code=500, detail=msg.DATABASE_ERROR)
+    schemas_posts = []
+    for post in posts: 
+        schemas_post = schemas.Post.from_orm(post)
+        schemas_post.get_user_detail(db=db)
+        schemas_posts.append(schemas_post)
             
-#     return posts
+    return schemas_posts    
+
 
 @router.get("/view/{post_id}", response_model=schemas.Post)
 def view_post(db: Session = Depends(deps.get_db), post_id:str = None, current_user: models.User = Depends(deps.get_current_user)) -> Any:
@@ -87,7 +88,9 @@ def view_post(db: Session = Depends(deps.get_db), post_id:str = None, current_us
     if not post:
         raise HTTPException(status_code=404, detail=msg.INVALID_POST_ID)
 
-    return post
+    schemas_post = schemas.Post.from_orm(post)
+    schemas_post.get_user_detail(db=db)
+    return schemas_post
 
 @router.post("/create", response_model=schemas.Post)
 def create_post(db: Session = Depends(deps.get_db), creating_post: PostCreate = None, current_user: models.User = Depends(deps.get_current_user)) -> Any:
@@ -101,7 +104,9 @@ def create_post(db: Session = Depends(deps.get_db), creating_post: PostCreate = 
             # user_id =  creating_post.user_id
             user_id=current_user.user_id
         )
-        return post
+        schemas_post = schemas.Post.from_orm(post)
+        schemas_post.get_user_detail(db=db)
+        return schemas_post
     except Exception as e:
         print(e)
         raise HTTPException(status_code=500, detail=msg.INVALID_POST_ID)
@@ -125,7 +130,9 @@ def update_post(db: Session = Depends(deps.get_db), updating_post: PostUpdate = 
         db_obj=query_post,
         obj_in=updating_post
     )
-    return post
+    schemas_post = schemas.Post.from_orm(post)
+    schemas_post.get_user_detail(db=db)
+    return schemas_post
 
     
 @router.delete("/delete", response_model=schemas.Post)
@@ -177,7 +184,13 @@ def search(db: Session = Depends(deps.get_db), *, searched_text: Optional[str] =
 
     if not isinstance(posts, List):
         raise HTTPException(status_code=500, detail=msg.DATABASE_ERROR)
-    return posts 
+
+    schemas_posts = []
+    for post in posts: 
+        schemas_post = schemas.Post.from_orm(post)
+        schemas_post.get_user_detail(db=db)
+        schemas_posts.append(schemas_post)
+    return schemas_posts    
 
 @router.get("/view-topics/{post_id}", response_model=List[schemas.Topic])
 def get_topic_title(db: Session = Depends(deps.get_db), *, post_id, current_user: models.User = Depends(deps.get_current_user)) -> Any:
